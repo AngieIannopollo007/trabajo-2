2e1439955889060e5a517becb40ff267129c3435
AngieIannopollo007 <167256745+AngieIannopollo007@users.noreply.github.com>
2025-05-04 21:41:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 const solPeruano = 400; 
 const reales = 300;
-const dolares = 1300;
+const dolar = 1300;
 const euro = 1400;
 const yen = 100;
+const franco = 2000;
+const pesoColombiano = 200;
+const quetzal = 500;
 
-function suma(){
+function suma(event){
     event.preventDefault();
     let pais = document.getElementById("opcion").value;
     let dias = parseFloat(document.getElementById("dias").value);
 
     let costoViaje = 100 * dias; 
-    let costoPesos = 0;
-    if(pais == "Cusco") {
-         costoPesos = costoViaje * solPeruano;
-    }if(pais == "Rio"){
-         costoPesos = costoViaje * reales;
-    }if(pais == "Miami"){
-         costoPesos = costoViaje * dolares;
-    }if(pais == "Barcelona"){
-         costoPesos = costoViaje * euro;
-    }if(pais == "Tokyo"){
-         costoPesos = costoViaje * yen;
-    }
+    let costoPesos;
+    if(pais == "Cusco") costoPesos = costoViaje * solPeruano;
+    if(pais == "Rio") costoPesos = costoViaje * reales;
+    if(pais == "Miami") costoPesos = costoViaje * dolar;
+    if(pais == "Barcelona") costoPesos = costoViaje * euro;
+    if(pais == "Tokyo") costoPesos = costoViaje * yen;
+    if(pais == "Suiza") costoPesos = costoViaje * franco;
+    if(pais == "Colombia") costoPesos = costoViaje * pesoColombiano;
+    if(pais == "Guatemala") costoPesos = costoViaje * quetzal;
+
 
     document.getElementById("costoFinal").textContent =
     "El numero es: " + costoPesos;
